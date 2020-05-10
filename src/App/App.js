c7e7a57576e644603a0b76b8ec22c8c7f06c8d2c
Zoe Ames <zoeames@gmail.com>
2020-05-10 18:14:01 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Scat Surprise</h2>
+        <button className="btn btn-dark"><i class="fas fa-poo fa-3x"></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
