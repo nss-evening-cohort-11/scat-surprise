e0f664d5400087e30df21cd47854708e32c7acbc
Zoe Ames <zoeames@gmail.com>
2020-06-06 11:24:55 -0500

    added buttons to scat cards

--- src/components/shared/ScatCard/ScatCard.js
+++ src/components/shared/ScatCard/ScatCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import scatShape from '../../../helpers/propz/scatShape';
 import './ScatCard.scss';
@@ -10,11 +11,15 @@ class ScatCard extends React.Component {
 
   render() {
     const { scat } = this.props;
+    const singleLink = `/scats/${scat.id}`;
+    const editLink = `/edit/${scat.id}`;
     return (
       <div className="ScatCard col-3">
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{scat.location}</h5>
+            <Link className="btn btn-info" to={singleLink}><i className="fas fa-binoculars"></i></Link>
+            <Link className="btn btn-warning" to={editLink}><i className="fas fa-pencil-alt"></i></Link>
             <p className="card-text">{scat.notes}</p>
           </div>
         </div>
