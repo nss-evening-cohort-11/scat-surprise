7af542d56d99f73da602bd3bd7cd3f6d079cd335
Zoe Ames <zoeames@gmail.com>
2020-06-09 18:26:54 -0500

    can delete from single view

--- src/components/pages/SingleScat/SingleScat.js
+++ src/components/pages/SingleScat/SingleScat.js
@@ -15,10 +15,18 @@ class SingleScat extends React.Component {
       .catch((err) => console.error('unable to get single scat: ', err));
   }
 
+  removeScat = () => {
+    const { scatId } = this.props.match.params;
+    scatsData.deleteScat(scatId)
+      .then(() => this.props.history.push('/home'))
+      .catch((err) => console.error('unable to delete scat: ', err));
+  }
+
   render() {
     const { scat } = this.state;
     return (
       <div className="SingleScat" style={{ backgroundColor: scat.color }}>
+        <button className="btn btn-danger" onClick={this.removeScat}><i className="fas fa-trash-alt"></i></button>
         <h1>{scat.location}</h1>
         <p>Shape: {scat.shape}</p>
         <p>Size: {scat.size}</p>
